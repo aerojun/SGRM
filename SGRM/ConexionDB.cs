ebedfbeb05ef0bec8c017728a50acfaef6621226
Jun Singht <juani.navarrog@gmail.com>
2013-10-06 22:47:07 -0500

    Primera
    Definicion de layout

---
+++ SGRM/ConexionDB.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SGRM
+{
+    class ConexionDB
+    {
+        /*
+         * CONEXION A LA BASE DE DATOS
+         */
+        static string conexion = "SERVER=localhost; DATABASE=hospital;"
+            + "UID=development; PASSWORD=x7uj2jsp;"; //Datos del servidor
+
+        MySqlConnection conexionn = new MySqlConnection(conexion); //Objeto de conexion
+
+        public ConexionDB()
+        {
+
+        }
+
+        public bool verificarUsuario(string usuario, string contrasena)
+        {
+            string query = "SELECT id FROM usuarios WHERE password = '" + contrasena + "' AND usuario = '" + usuario + "'";
+            MySqlCommand enviarQuery = new MySqlCommand(query, conexionn);
+
+            try
+            {
+                //Abrir conexion
+                conexionn.Open();
+
+                //Enviar query
+                MySqlDataReader resultadoQuery = enviarQuery.ExecuteReader();
+
+                if (resultadoQuery.HasRows)
+                    return true;
+                else
+                    return false;
+
+                //Finalizo correctamente
+            }
+            catch (MySqlException MySqlError)
+            {
+                return false;
+            }
+            finally
+            {
+                //Cerrar conexion
+                conexionn.Close();
+            }
+        }
+
+        public bool agregarAlumno(int id, string Nombre, string Apellido)
+        {
+            string query = "INSERT INTO Alumnos (id, Nombre, Apellidos) VALUES ('" + id + "','" + Nombre + "','" + Apellido + "')";
+            MySqlCommand enviarQuery = new MySqlCommand(query, conexionn);
+
+            try
+            {
+                //Abrir conexion
+                conexionn.Open();
+
+                //Enviar query
+                enviarQuery.ExecuteNonQuery();
+
+                //Finalizo correctamente
+                return true;
+            }
+            catch (MySqlException MySqlError)
+            {
+                return false;
+            }
+            finally
+            {
+                //Cerrar conexion
+                conexionn.Close();
+            }
+        }
+
+        public bool eliminarAlumno()
+        {
+            return false;
+        }
+    }
+}
---
+++ SGRM/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.ComponentModel;
+
+
+namespace SGRM
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private string usuario = "";
+        private string password = "";
+        BackgroundWorker backgroundWorker1 = new BackgroundWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += delegate
+            {
+                Dispatcher.BeginInvoke(new Action(delegate { progreso.Visibility = System.Windows.Visibility.Visible; }));
+                Dispatcher.BeginInvoke(new Action(delegate { Verificar(); }));
+            };
+            worker.RunWorkerAsync();
+        }
+      
+
+        public void Verificar()
+        {
+            /*
+             * Obtiene los datos de los textbox
+             */
+            usuario = user.Text;
+            password = pass.Password;
+            /*
+             * Limpia los textbox
+             */
+            user.Clear();
+            pass.Clear();
+
+            ConexionDB conectar = new ConexionDB();
+            if (conectar.verificarUsuario(usuario, password))
+            {
+                var control = new Controles();
+                mostrarControl();
+                progreso.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else
+            {
+                MessageBox.Show("Acceso Denegado");
+                progreso.Visibility = System.Windows.Visibility.Hidden;
+            }
+
+        }
+
+        public void mostrarControl()
+        {
+            this.Hide();
+            var segundaFase = new Controles();
+            segundaFase.Show();
+            segundaFase.Closing += Controles_Closing;
+        }
+
+        private void Controles_Closing(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+    }
+}
